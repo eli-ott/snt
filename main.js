e1fded4bb2634c7a66cba80c2d09d1fddbda36fc
eli-ott <74316463+eli-ott@users.noreply.github.com>
2020-11-13 17:42:07 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
-//we create a loading animation
-$(window).on("load", () => {
-    console.log("loading" + new Date().getMilliseconds());
-    $("head").prepend("<link rel='stylesheet' href='loader.css'>");
-});
-
-$(window).ready(() => {
-    console.log("loaded" + new Date().getMilliseconds());
-    $("head").find("link")[0].remove();
-});
-
 //display the menu when the button is clicked
 $("#menu").on("click", () => {
     $("#menuContent").slideDown();
